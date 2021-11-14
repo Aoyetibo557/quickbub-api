dc0921df5df9f31d3b9714265e2175cb06812b5b
aoyetibo557 <aoyetibo@gmail.com>
2021-11-14 14:54:29 -0500

    first commit

--- config/db.config.js
+++ config/db.config.js
@@ -1,10 +1,27 @@
 
+// module.exports = {
+//   HOST: "localhost",
+//   USER: "root",
+//   PASSWORD: "",
+//   DB: "quickbuck_test",
+//   PORT: 3307,
+//   dialect: "mysql",
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     acquire: 30000,
+//     idle: 10000
+//   }
+// };
+
+
+// ClearDb Connection
 module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "",
-  DB: "quickbuck_test",
-  PORT: 3307,
+  HOST: "us-cdbr-east-04.cleardb.com",
+  USER: "b6cab839e2aee8",
+  PASSWORD: "d6757ff4",
+  DB: "heroku_de5533936e51148",
+  PORT: 3306,
   dialect: "mysql",
   pool: {
     max: 5,
@@ -12,4 +29,4 @@ module.exports = {
     acquire: 30000,
     idle: 10000
   }
-};
+}
